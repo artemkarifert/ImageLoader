ccc490a59306358d2911d74d8d3479936d4ccb5b
artemkarifert <artem.rifert@gmail.com>
2020-12-21 17:47:10 +0700

    first works version

--- app/src/main/java/com/example/game/GameScreen.java
+++ app/src/main/java/com/example/game/GameScreen.java
@@ -4,49 +4,56 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.CountDownTimer;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.View;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class GameScreen extends View {
+public class GameScreen extends SurfaceView implements SurfaceHolder.Callback{
 
-    class Timer extends CountDownTimer {
-
-        public Timer(){
-            super(Integer.MAX_VALUE, 1000/60);
-        }
-
-        public void onTick(long l){
-            GameScreen.this.invalidate();
-        }
-
-        public void onFinish(){
-
-        }
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+        setupThread(getContext());
     }
 
-    Timer timer;
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    GameThread gameThread;
 
     public GameScreen(Context context) {
         super(context);
-        timer = new Timer();
-        timer.start();
+        getHolder().addCallback(this);
+    }
+
+    private void setupThread(Context context) {
+        gameThread = new GameThread();
+        gameThread.setSurfaceHolder(getHolder());
+        gameThread.setContext(context);
+        gameThread.start();
     }
 
     public GameScreen(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        timer = new Timer();
-        timer.start();
+        getHolder().addCallback(this);
     }
 
     public GameScreen(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        timer = new Timer();
-        timer.start();
+        getHolder().addCallback(this);
     }
 
     float goalX; float goalY;
@@ -55,36 +62,78 @@ public class GameScreen extends View {
     private float personY = 0;
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        Bitmap person = BitmapFactory.decodeResource(getResources(), R.drawable.bird);
-        person = Bitmap.createScaledBitmap(person, 200, 200, false);
-
-        Paint paint = new Paint();
-
-        canvas.drawBitmap(person, personX, personY, paint);
-        if(personX < goalX)
-            personX += 1;
-
-        else
-            personX -= 1;
-
-
-        if (personY < goalY)
-            personY += 1;
-
-        else
-            personY -= 1;
-    }
-
-    @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN){
-            goalX = event.getX();
-            goalY = event.getY();
+            gameThread.setGoal(event.getX(), event.getY());
         }
 
         return super.onTouchEvent(event);
+    }
+}
+class GameThread extends Thread{
+
+    private SurfaceHolder surfaceHolder;
+    private Context context;
+    private float personX = 0;
+    private float personY = 0;
+    private float goalX = 0;
+    private float goalY = 0;
+    boolean running = true;
+
+
+    public void setSurfaceHolder(SurfaceHolder surfaceHolder) {
+        this.surfaceHolder = surfaceHolder;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public void setGoal(float goalX, float goalY) {
+        this.goalX = goalX;
+        this.goalY = goalY;
+    }
+
+    public void stopDraw(){
+        running = false;
+    }
+
+
+    @Override
+    public void run() {
+
+        Bitmap person = BitmapFactory.decodeResource(context.getResources(), R.drawable.bird);
+        person = Bitmap.createScaledBitmap(person, 200, 200, false);
+
+        while (running) {
+            Canvas canvas = surfaceHolder.lockCanvas();
+
+            if(canvas != null) {
+                canvas.drawColor(Color.WHITE);
+
+                Paint paint = new Paint();
+
+                canvas.drawBitmap(person, personX, personY, paint);
+                if (personX < goalX)
+                    personX += 10;
+
+                else if (personX > goalX)
+                    personX -= 10;
+
+
+                if (personY < goalY)
+                    personY += 10;
+
+                else if(personY > goalY)
+                    personY -= 10;
+
+                if(Math.abs(personX - goalX) < 10)
+                    personX = goalX;
+                if(Math.abs(personY - goalY) < 10)
+                    personY = goalY;
+
+                surfaceHolder.unlockCanvasAndPost(canvas);
+            }
+        }
     }
 }
